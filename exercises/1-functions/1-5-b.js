0638de77dce72ceac81b052e0d2c7c77b5d47d23
Meryam Belaouchi <meryam.belaouchi@student.ehb.be>
2022-10-05 16:08:49 +0200

    ex 5 b  done

---
+++ exercises/1-functions/1-5-b.js
@@ -0,0 +1,31 @@
+"use strict";
+
+let canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext('2d');
+
+drawSunset();
+
+function drawSunset() {
+
+
+
+    context.fillStyle = 'orange';
+    context.beginPath();
+    context.rect(50, 50, 300, 300);
+    context.fill();
+
+    context.fillStyle = 'yellow';
+    context.beginPath();
+    context.ellipse(200, 250, 100, 50, 0, 0, 2 * Math.PI);
+    context.fill();
+
+    context.fillStyle = 'blue';
+    context.beginPath();
+    context.rect(50, 250, 300, 100);
+    context.fill();
+
+
+
+}
