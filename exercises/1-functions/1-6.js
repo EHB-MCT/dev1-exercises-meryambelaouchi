d0d1ba6b51c1fbd91bf01f8a90b97af45fa6a689
Meryam Belaouchi <meryam.belaouchi@student.ehb.be>
2022-10-08 17:17:41 +0200

    ex 1-6 done

--- exercises/1-functions/1-6.js
+++ exercises/1-functions/1-6.js
@@ -11,64 +11,70 @@ function drawStar() {
 
     context.fillStyle = 'orange';
     context.beginPath();
-    context.rect(50, 50, 300, 300);
+    context.rect(50, 50, 350, 350);
     context.fill();
 
     context.lineWidth = 7;
     context.strokeStyle = 'white';
     context.beginPath();
     context.moveTo(50, 50);
-    context.lineTo(175, 175);
+    context.lineTo(200, 200);
 
-    context.moveTo(225, 225);
-    context.lineTo(350, 350);
+    context.moveTo(250, 250);
+    context.lineTo(400, 400);
 
     context.moveTo(100, 50);
-    context.lineTo(350, 300);
+    context.lineTo(400, 350);
     context.moveTo(150, 50);
-    context.lineTo(350, 250);
+    context.lineTo(400, 300);
     context.moveTo(50, 100);
-    context.lineTo(300, 350);
+    context.lineTo(350, 400);
     context.moveTo(50, 150);
-    context.lineTo(250, 350);
+    context.lineTo(300, 400);
+
+    context.moveTo(400, 50);
+    context.lineTo(250, 200);
+
+    context.moveTo(200, 250);
+    context.lineTo(50, 400);
 
     context.moveTo(350, 50);
-    context.lineTo(225, 175);
-
-    context.moveTo(175, 225);
     context.lineTo(50, 350);
-
     context.moveTo(300, 50);
     context.lineTo(50, 300);
-    context.moveTo(250, 50);
-    context.lineTo(50, 250);
-    context.moveTo(350, 100);
-    context.lineTo(100, 350);
-    context.moveTo(350, 150);
-    context.lineTo(150, 350);
+    context.moveTo(400, 100);
+    context.lineTo(100, 400);
+    context.moveTo(400, 150);
+    context.lineTo(150, 400);
     context.stroke();
 
     context.fillStyle = 'white';
     context.beginPath();
-    context.arc(50, 50, 150, 0, Math.PI);
+    context.arc(50, 50, 175, 0, Math.PI);
     context.fill();
 
     context.beginPath();
-    context.arc(350, 50, 150, 0, Math.PI);
+    context.arc(400, 50, 175, 0, Math.PI);
     context.fill();
 
     context.beginPath();
-    context.arc(50, 350, 150, Math.PI, 0);
+    context.arc(50, 400, 175, 0, Math.PI, true);
     context.fill();
 
     context.beginPath();
-    context.arc(350, 350, 150, Math.PI, 0);
+    context.arc(400, 400, 175, 0, Math.PI, true);
     context.fill();
 
     context.lineWidth = 10;
     context.strokeStyle = 'orange';
+    context.fillStyle = "orange";
+    context.textAlign = "center";
+    context.font = "normal 35pt Arial";
     context.beginPath();
-    context.rect(50, 50, 300, 300);
+    context.rect(50, 50, 350, 350);
+    context.fillText('STARRY NIGHT', 225, 450);
     context.stroke();
+
+
 
 }
