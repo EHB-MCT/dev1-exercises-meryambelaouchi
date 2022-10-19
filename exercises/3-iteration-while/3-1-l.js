a009505256277a51820b5540d6b08f9f1d54ef4d
Meryam Belaouchi <meryam.belaouchi@student.ehb.be>
2022-10-19 15:57:21 +0200

    3-1-l done

--- exercises/3-iteration-while/3-1-l.js
+++ exercises/3-iteration-while/3-1-l.js
@@ -14,8 +14,8 @@ function draw() {
 
 function drawHourGlass() {
     let i = 0;
-    while () {
-        Utils.drawLine(, , , );
+    while (i < 31) {
+        Utils.drawLine(50 + (i * 10), 50, 350 - (i * 10), 350);
         i++;
     }
 }
