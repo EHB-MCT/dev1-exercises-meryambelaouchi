e66c60c97ceee112d5d74d5a78add60ca2739acd
Meryam Belaouchi <meryam.belaouchi@student.ehb.be>
2022-10-22 17:29:35 +0200

    3-1-s done

--- exercises/3-iteration-while/3-1-s.js
+++ exercises/3-iteration-while/3-1-s.js
@@ -12,9 +12,9 @@ function draw() {
 function drawShrinkingBars() {
     let i = 0;
     while (i <= 14) {
-        let gray = ;
+        let gray = 250 - i * 25;
         context.fillStyle = Utils.rgb(gray, gray, gray);
-        context.fillRect(, , , );
+        context.fillRect(50 + i * 25, 50 + i * 15, 15, 350 - i * 25);
         i++;
     }
 }
