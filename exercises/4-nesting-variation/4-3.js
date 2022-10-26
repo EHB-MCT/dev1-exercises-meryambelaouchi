b0d7c36a425492b879008cf088562b836f2b2479
Meryam Belaouchi <meryam.belaouchi@student.ehb.be>
2022-10-26 15:45:43 +0200

    4-3 bricks done

---
+++ exercises/4-nesting-variation/4-3.js
@@ -0,0 +1,21 @@
+"use strict";
+import context from "../../scripts/context.js";
+import * as Utils from "../../scripts/utils.js";
+
+drawBricks();
+
+function drawBricks() {
+    for (let i = 0; i <= 5; i++) {
+        for (let j = 0; j <= 2; j++) {
+            context.fillStyle = '#8B0000';
+            context.fillRect(50 + i * 105, 50 + j * 110, 100, 50);
+        }
+    }
+
+    for (let i = 0; i <= 5; i++) {
+        for (let j = 0; j <= 2; j++) {
+            context.fillStyle = '#8B0000';
+            context.fillRect(100 + i * 105, 105 + j * 110, 100, 50);
+        }
+    }
+}
